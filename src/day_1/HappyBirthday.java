e6b6a116cf661869421eab0b769253f1643370ac
Aysa86 <64173070+Aysa86@users.noreply.github.com>
2020-04-28 15:53:57 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,5 +3,11 @@ package day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday");
+        
+        
+        
+       System.out.println("My best wishes!!!")// this line was added from GitHub 
+        
+        
     }
 }
